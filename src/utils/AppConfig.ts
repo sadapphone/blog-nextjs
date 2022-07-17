12e33b9b68ae922e61d23706e3740d0e5e8c5cc6
sadapphone <108273997+sadapphone@users.noreply.github.com>
2022-07-18 04:46:25 +0700

    Add files via upload

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,10 @@
+export const AppConfig = {
+  site_name: 'Contenty',
+  title: 'Konten Boilerplate',
+  description:
+    'Starter code for your articles blog Boilerplate with NextJS',
+  url: 'https://example.com',
+  locale: 'id',
+  author: 'Anonymous',
+  pagination_size: 5,
+};
